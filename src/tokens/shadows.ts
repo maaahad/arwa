48a2072bd673b090163a9c95f629b2f806397bd2
Muhammed Ahad <maaahad@gmail.com>
2023-07-01 23:28:38 +0200

    Shadows for dark theme

--- src/tokens/shadows.ts
+++ src/tokens/shadows.ts
@@ -1,17 +1,16 @@
 
 export enum Light {
     xs = '0 4px 12px -6px rgb(0 0 0 / 0.06)',
-    sm = '0 6px 6px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    md = '0 12px 12px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    lg = '0 8px 24px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    xl = '0 32px 32px -8px rgb(0 0 0 / 0.08), 0 0 32px -8px rgb(0 0 0 / 0.12), 0 0 1px rgb(0 0 0 / 0.2)',
+    sm = '0px 6px 6px -6px rgba(0, 0, 0, 0.16), 0px 0px 1px rgba(0, 0, 0, 0.4)',
+    md = '0px 12px 12px -6px rgba(0, 0, 0, 0.16), 0px 0px 1px rgba(0, 0, 0, 0.4)',
+    lg = '0px 8px 24px -6px rgba(0, 0, 0, 0.16), 0px 0px 1px rgba(0, 0, 0, 0.4)',
+    xl = '0px 32px 32px -8px rgba(0, 0, 0, 0.08), 0px 0px 32px -8px rgba(0, 0, 0, 0.12), 0px 0px 1px rgba(0, 0, 0, 0.2)',
 }
 
-// FIXME: This might need to be changed to something different
 export enum Dark {
     xs = '0 4px 12px -6px rgb(0 0 0 / 0.06)',
-    sm = '0 6px 6px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    md = '0 12px 12px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    lg = '0 8px 24px -6px rgb(0 0 0 / 0.16), 0 0 1px rgb(0 0 0 / 0.4)',
-    xl = '0 32px 32px -8px rgb(0 0 0 / 0.08), 0 0 32px -8px rgb(0 0 0 / 0.12), 0 0 1px rgb(0 0 0 / 0.2)',
+    sm = '0px 6px 6px -6px rgba(0, 0, 0, 0.64), 0px 0px 1px rgba(0, 0, 0, 0.56)',
+    md = '0px 12px 12px -6px rgba(0, 0, 0, 0.64), 0px 0px 1px rgba(0, 0, 0, 0.56)',
+    lg = '0px 24px 24px -6px rgba(0, 0, 0, 0.64), 0px 0px 1px rgba(0, 0, 0, 0.56)',
+    xl = '0px 48px 48px -6px rgba(0, 0, 0, 0.88), 0px 0px 1px rgba(0, 0, 0, 0.72)',
 }
