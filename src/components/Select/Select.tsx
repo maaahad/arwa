0f7bb8e212e621db4cd11bf175677d2e394b878f
Muhammed Ahad <maaahad@gmail.com>
2023-10-20 23:21:41 +0200

    Select: wip

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { InputContainer } from "../Helper/styled"
+
+const Select: React.FC<{}> = () => {
+    return (
+        <InputContainer>
+            <div>Select</div>
+            <div>Caret</div>
+        </InputContainer>
+    )
+}
+
+
+export default Select
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Card } from "./Card";
 export { default as Carousel } from "./Carousel";
 export { default as Breadcrumb } from "./Breadcrumb";
 export { default as Search } from "./Search";
+export { default as Select} from "./Select";
