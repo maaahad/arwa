11d8951aa5fee22a74b213f213da49b76e504753
Muhammed Ahad <maaahad@gmail.com>
2023-11-26 00:04:26 +0100

    format

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,59 +1,49 @@
 import React, { ReactElement, ReactNode, createContext, useState } from "react";
 
-
-
 type TabsProps = {
-    children?: ReactNode | undefined
-}
-
+  children?: ReactNode | undefined;
+};
 
 type ExtendedTabsProps<T extends Record<string, any>> = React.FC<T> & {
-    Tab?: React.FC<Record<string, any>> | ReactNode | undefined // TODO(maaahad): this type needs to be updated
-    TabContent?: React.FC<Record<string, any>> | ReactNode | undefined // TODO(maaahad): this type needs to be updated
-}
+  Tab?: React.FC<Record<string, any>> | ReactNode | undefined; // TODO(maaahad): this type needs to be updated
+  TabContent?: React.FC<Record<string, any>> | ReactNode | undefined; // TODO(maaahad): this type needs to be updated
+};
 
 const TabsContext = createContext<{
-    activeIndex: number,
-    setActiveIndex?: (index: number) => void
+  activeIndex: number;
+  setActiveIndex?: (index: number) => void;
 }>({
-    activeIndex: 0, 
-    setActiveIndex: () => {}
-})
+  activeIndex: 0,
+  setActiveIndex: () => {},
+});
 
-const Tabs: ExtendedTabsProps<TabsProps> = ({children}) => {
-    const [activeIndex, setActiveIndex] = useState<number>(0)
+const Tabs: ExtendedTabsProps<TabsProps> = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
-    return (
-        <TabsContext.Provider value={{
-            activeIndex, 
-            setActiveIndex
-        }}>
-            {children}
-        </TabsContext.Provider>
-    )
-}
-
+  return (
+    <TabsContext.Provider
+      value={{
+        activeIndex,
+        setActiveIndex,
+      }}
+    >
+      {children}
+    </TabsContext.Provider>
+  );
+};
 
 // TODO(maaaahd): TabContent
 
-const Tab: React.FC<{label: string}> = ({label}) => {
-    const []
-    return (
-        <div>
-            {label}
-        </div>
-    )
-}
+const Tab: React.FC<{ label: string }> = ({ label }) => {
+  const [];
+  return <div>{label}</div>;
+};
 
 const TabContent: React.FC<{}> = () => {
-    return (
-        <div>
-            TabContent
-        </div>
-    )
-}
+  return <div>TabContent</div>;
+};
 
-Tabs.Tab = Tab 
-Tabs.TabContent = TabContent 
+Tabs.Tab = Tab;
+Tabs.TabContent = TabContent;
 
-export default Tabs
+export default Tabs;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,5 @@ export { default as Card } from "./Card";
 export { default as Carousel } from "./Carousel";
 export { default as Breadcrumb } from "./Breadcrumb";
 export { default as Search } from "./Search";
-export { default as Select} from "./Select";
-export { default as Tabs } from './Tabs'
+export { default as Select } from "./Select";
+export { default as Tabs } from "./Tabs";
